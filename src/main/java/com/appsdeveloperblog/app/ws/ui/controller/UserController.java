1107777a1ac22a733b0ed990d63e4140e2e34c58
Sean <jordaansean7@gmail.com>
2022-09-01 14:01:15 +0200

    first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
+@RestController
+@RequestMapping("/users")//http://localhost8080/users
+public class UserController {
+	
+	@GetMapping()
+	public String getUsers(@RequestParam(value = "page", defaultValue = "1")int page,
+			@RequestParam(value = "limit", defaultValue = "50")int limit,
+			@RequestParam(value = "sort", defaultValue = "desc",  required = false)String sort) {
+		return "get users was called with page =" + page + "and limit = " + limit + " and sort = " + sort;
+	}
+
+	@GetMapping(path = "/{userId}",
+			produces = {
+					//MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity <UserRest>  getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("Sean");
+		returnValue.setLastName("Jordaan");
+		
+		return new ResponseEntity <UserRest> (returnValue ,HttpStatus.OK);
+	}
+	
+	@PostMapping /*This stuff with the att sign is an anitaion*/
+	public String createUser() {
+		return " createUser was called";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "updateUser was called"; 
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "deleteUser was called"; 
+	}
+	
+}
